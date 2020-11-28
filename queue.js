13aa8fe807c9812b5e9ad016600f3cdc94a7bea7
geeth22 <geethchandra22@gmail.com>
2020-11-29 01:37:55 +0530

    refactor:check option for last element

--- queue.js
+++ queue.js
@@ -3,7 +3,7 @@ let readlineSync = require('readline-sync')
 checkoption = function () {
     let isTerminated = true;
     while (isTerminated == true) {
-        let checkoption = readlineSync.question("\nEner your choice:\n1.enqueue\n2.dequeue\n3.print\n4.frontElement\n5.exit\n");
+        let checkoption = readlineSync.question("\nEner your choice:\n1.enqueue\n2.dequeue\n3.print\n4.frontElement\n5.lastElement\n6.exit\n");
         switch (checkoption) {
             case '1':
                 let element = readlineSync.question("Enter the element:");
@@ -20,6 +20,10 @@ checkoption = function () {
                 console.log(frontElement);
                 break;
             case '5':
+                lastElement = Utility.getLast();
+                console.log(lastElement);
+                break;
+            case '6':
                 isTerminated = false;
                 break;
             default: console.log("please enter correct option");
