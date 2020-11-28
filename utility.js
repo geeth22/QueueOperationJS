8f95545dd0b8805b61e564120ee0278b9429fce6
geeth22 <geethchandra22@gmail.com>
2020-11-29 01:17:15 +0530

    added:function to enqueue element

--- utility.js
+++ utility.js
@@ -1,2 +1,19 @@
-//Welcome to Queue Operations
-//enqueue, dequeue, front element, last element
+class Queue {
+   
+   constructor(){
+     
+     this.data = [];
+     this.rear = 0;
+     this.size = 10;
+   }
+   enqueue(element) {
+   
+	//need to check if the queue is full or not so it avoids overflow
+    if(this.rear < this.size ) {
+          this.data[this.rear] = element;
+          this.rear = this.rear + 1;
+     }
+  }
+}
+module.exports = new Queue();
+
